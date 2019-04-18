4a816c8537b4b5919883322523f22ef601bca749
rickingyn <rickingyn@gmail.com>
2019-04-18 12:07:02 -0400

    clean up code
    reorganize code

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 // assign 'Other' input to a variable and hide 
 const otherTitle = $('#other-title').hide();
+// create variable to calculate running total cost
+let total = 0;
 
-// select name text field and set focus 
-$('#name').focus();
-
+// create running total element for 'activities' section and hide
 let $total = $('<p></p>');
 $('.activities').append($total);
 $total.hide();
+
+// select name text field and set focus 
+$('#name').focus();
 
 // create function to show design color based on design option selected
 // change value shown in the select element to be the first option in the updated list
@@ -38,22 +41,6 @@ const showDesignColor = (design) => {
 		$('#color').val($('#colors-js-puns option').eq(0).val())
 	}
 };
-
-// add event listener on change in dropdown menu; 
-	// if 'other' option is selected, show 'other title' text field
-$('#title').on('change', () => {
-	if($('#title option:selected').text() === 'Other') {
-		otherTitle.show();
-	}
-});
-
-// add event listener on the 'Design' select element
-// call the showDesignColor function;
-	// set the 'Color' select element to show options based on selected 	
-$('#design').on('change', () => {
-	let selectedDesign = $('#design option:selected').val();
-	showDesignColor(selectedDesign);
-});
 
 // create function to check day and time of the activity
 // returns a regex for the activity;
@@ -88,8 +75,60 @@ const getRegex = (activity) => {
 		}
 	}
 };
-	// create variable to calculate total cost
-	let total = 0;
+
+// create function to add to running total based on selected activity
+const addTotal = text => {
+	// conditional statement to add to total price;
+		// 'Main Conference' price is $200
+		// All other activity's price is $100
+	if(text.includes('Main')) {
+		total += 200;
+	} else {
+		total += 100;
+	}
+		return total;
+};
+
+// create function to subract from running total based on selected activity
+const subtractTotal = text => {
+	// conditional statement to add to total price;
+		// 'Main Conference' price is $200
+		// All other activity's price is $100
+	if(text.includes('Main')) {
+		total -= 200;
+	} else {
+		total -= 100;
+	}
+		return total;
+};
+
+// function to display running total if there is an amount to display
+const displayTotal = total => {
+	// display total if activity is selected
+	if(total > 0) {
+		$total.show();
+		$total.text(`Total: $${total}`);
+	} else {
+		// hide total if no activity is selected
+		$total.hide();
+	}
+};
+
+// add event listener on change in dropdown menu; 
+	// if 'other' option is selected, show 'other title' text field
+$('#title').on('change', () => {
+	if($('#title option:selected').text() === 'Other') {
+		otherTitle.show();
+	}
+});
+
+// add event listener on the 'Design' select element
+// call the showDesignColor function;
+	// set the 'Color' select element to show options based on selected 	
+$('#design').on('change', () => {
+	let selectedDesign = $('#design option:selected').val();
+	showDesignColor(selectedDesign);
+});
 
 // add event listner on activitie's checkbox
 $('.activities input[type="checkbox"]').on('change', function() {
@@ -126,41 +165,7 @@ $('.activities input[type="checkbox"]').on('change', function() {
 			$(this).css('color', 'black');
 		});
 	}
-
+	// call function to display total (if there is amount to show)
 	displayTotal(total);
 });
 
-function addTotal(text) {
-	// conditional statement to add to total price;
-		// 'Main Conference' price is $200
-		// All other activity's price is $100
-	if(text.includes('Main')) {
-		total += 200;
-	} else {
-		total += 100;
-	}
-		return total;
-}
-
-function subtractTotal(text) {
-	// conditional statement to add to total price;
-		// 'Main Conference' price is $200
-		// All other activity's price is $100
-	if(text.includes('Main')) {
-		total -= 200;
-	} else {
-		total -= 100;
-	}
-		return total;
-}
-
-function displayTotal(total) {
-	// display total if activity is selected
-	if(total > 0) {
-		$total.show();
-		$total.text(`Total: $${total}`);
-	} else {
-		// hide total if no activity is selected
-		$total.hide();
-	}
-}
